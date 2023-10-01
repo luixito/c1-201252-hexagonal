b4885659a80a310800deeda9eb0778f2f13d0499
luis daniel molina <55844181+luixito@users.noreply.github.com>
2023-10-01 04:05:57 -0600

    Update index.ts
    normalizacion con cambios en reviews

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { bookRoutes } from "./books/infrastructure/bookRoutes";
+import { bookRoutes } from "./review/infrastructure/bookRoutes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1/books/', bookRoutes);
+app.use('/api/v1/books/', reviewRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 app.listen(port, () => {
   console.log(`funcionando desde: ${port}`);
 });
