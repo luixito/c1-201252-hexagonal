b7bf035ec541bb0b00e3d303cc3341816313dd8d
luis daniel molina <55844181+luixito@users.noreply.github.com>
2023-10-01 15:31:20 -0600

    Update index.ts
    correcciones index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import { userRoutes } from "./users/infrastructure/userRoutes";
+import { bookRoutes } from "./books/infrastructure/bookRoutes";
 
 const app = express();
 
@@ -9,7 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/v1/users/', userRoutes);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 app.listen(port, () => {
   console.log(`funcionando desde: ${port}`);
 });
+
